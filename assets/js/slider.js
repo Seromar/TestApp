23e94bb9ff14a10390ebd3033650e58a19166b55
Seromar <89927023+Seromar@users.noreply.github.com>
2024-10-25 14:19:53 +0300

    Add files via upload

---
+++ assets/js/slider.js
@@ -0,0 +1,18 @@
+slider1 = new Swiper('.slider1', {
+    freeMode: true,
+    direction: 'horizontal',
+    mousewheel: true,
+    slidesPerView: 2.75,
+    slidesOffsetBefore: 24,
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+    autoplay: {
+        delay: 2000,
+    },
+    navigation: {
+        nextEl: '.swiper-next',
+        prevEl: '.swiper-prev',
+    },
+    hashNavigation: true,
+})
