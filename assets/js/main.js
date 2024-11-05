d185fd98cddd698abc9a13ec22ecdc20e9545094
Seromar <89927023+Seromar@users.noreply.github.com>
2024-11-05 14:49:48 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+let menuBtn = document.querySelector('.menu_btn');
+let Bur = document.querySelector('.header_menu-burger');
+let Body = document.querySelector('body');
+
+menuBtn.addEventListener('click', function(){
+    menuBtn.classList.toggle('active');
+    Bur.classList.toggle('active');
+    Body.classList.toggle('lock');
+})
+
+document.getElementById("defaultOpen").click();
+
+
